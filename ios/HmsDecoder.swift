68ea3cb9930919f08f30eecb84755f2c24b365c7
Lavi Moolchandani <lavim@geekyants.com>
2021-07-19 16:19:38 +0530

    replace NSDictionary with [String: Any] type

--- ios/HmsDecoder.swift
+++ ios/HmsDecoder.swift
@@ -2,72 +2,72 @@ import HMSSDK
 import Foundation
 
 class HmsDecoder: NSObject {
-    static func getHmsRoom (_ room: HMSRoom) -> NSDictionary {
+    static func getHmsRoom (_ room: HMSRoom) -> [String: Any] {
         let id: String = room.id
         let name: String = room.name
         let metaData: String = room.metaData ?? ""
-        var peers: [NSDictionary] = []
+        var peers: [[String: Any]] = []
         
         for peer in room.peers {
             peers.append(getHmsPeer(peer))
         }
         
-        let result:NSDictionary = ["id": id, "name": name, "metaData": metaData, "peers": peers]
+        let result:[String: Any] = ["id": id, "name": name, "metaData": metaData, "peers": peers]
         return result
         
     }
     
-    static func getHmsPeer (_ peer: HMSPeer) -> NSDictionary {
+    static func getHmsPeer (_ peer: HMSPeer) -> [String: Any] {
         let peerID: String = peer.peerID
         let name: String = peer.name
         let isLocal: Bool = peer.isLocal
         let customerUserID: String = peer.customerUserID ?? ""
         let customerDescription: String = peer.customerDescription ?? ""
-        let audioTrack: NSDictionary = getHmsAudioTrack(peer.audioTrack)
-        let videoTrack : NSDictionary = getHmsVideoTrack(peer.videoTrack)
-        var auxilaryTracks: [NSDictionary] = []
+        let audioTrack: [String: Any] = getHmsAudioTrack(peer.audioTrack)
+        let videoTrack : [String: Any] = getHmsVideoTrack(peer.videoTrack)
+        var auxilaryTracks: [[String: Any]] = []
         
         for track in peer.auxiliaryTracks ?? [] {
             auxilaryTracks.append(getHmsTrack(track))
         }
         
-        let result:NSDictionary = ["peerID": peerID, "name": name, "isLocal": isLocal, "customerUserID": customerUserID, "customerDescription": customerDescription, "audioTrack": audioTrack, "videoTrack": videoTrack, "auxilaryTracks": auxilaryTracks]
+        let result:[String: Any] = ["peerID": peerID, "name": name, "isLocal": isLocal, "customerUserID": customerUserID, "customerDescription": customerDescription, "audioTrack": audioTrack, "videoTrack": videoTrack, "auxilaryTracks": auxilaryTracks]
 
         return result
     }
     
-    static func getHmsTrack (_ track: HMSTrack?) -> NSDictionary {
+    static func getHmsTrack (_ track: HMSTrack?) -> [String: Any] {
         if let hmsTrack = track {
             let trackId: String = hmsTrack.trackId
             let source: HMSTrackSource = hmsTrack.source
             let trackDescription: String = hmsTrack.trackDescription
             
-            let result:NSDictionary  = ["trackId": trackId, "source": source, "trackDescription": trackDescription]
+            let result:[String: Any]  = ["trackId": trackId, "source": source, "trackDescription": trackDescription]
             return result;
         } else {
             return [:];
         }
     }
     
-    static func getHmsAudioTrack (_ hmsAudioTrack: HMSAudioTrack?) -> NSDictionary {
+    static func getHmsAudioTrack (_ hmsAudioTrack: HMSAudioTrack?) -> [String: Any] {
         if let hmsTrack = hmsAudioTrack {
             let trackId: String = hmsTrack.trackId
             let source: HMSTrackSource = hmsTrack.source
             let trackDescription: String = hmsTrack.trackDescription
             
-            let result:NSDictionary  = ["trackId": trackId, "source": source, "trackDescription": trackDescription]
+            let result:[String: Any]  = ["trackId": trackId, "source": source, "trackDescription": trackDescription]
             return result;
         } else {
             return [:]
         }
     }
     
-    static func getHmsVideoTrack (_ hmsVideoTrack: HMSVideoTrack?) -> NSDictionary {
+    static func getHmsVideoTrack (_ hmsVideoTrack: HMSVideoTrack?) -> [String: Any] {
         if let hmsTrack = hmsVideoTrack {
             let trackId: String = hmsTrack.trackId
             let source: HMSTrackSource = hmsTrack.source
             let trackDescription: String = hmsTrack.trackDescription
-            let result : NSDictionary = ["trackId": trackId, "source": source, "trackDescription": trackDescription]
+            let result : [String: Any] = ["trackId": trackId, "source": source, "trackDescription": trackDescription]
             return result;
         } else {
             return [:]
