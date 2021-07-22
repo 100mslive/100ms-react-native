24f25cf286574174ed3fdb2c3be39f9c30bec14e
Yogesh Singh <yogesh.singh.2810@gmail.com>
2021-07-22 22:35:45 +0530

    added firebase

--- example/ios/HmssdkExample/AppDelegate.m
+++ example/ios/HmssdkExample/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+@import Firebase;
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -48,6 +49,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [FIRApp configure];
   return YES;
 }
 
