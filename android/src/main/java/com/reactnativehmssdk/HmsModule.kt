e705e97e730916a67d89787a4f818791bf9b7716
Samarth Jain <samarthjain@Samarth-JainI112s-MacBook-Pro.local>
2021-08-16 17:43:22 +0530

    implemented switch camera function

--- android/src/main/java/com/reactnativehmssdk/HmsModule.kt
+++ android/src/main/java/com/reactnativehmssdk/HmsModule.kt
@@ -13,6 +13,7 @@ import live.hms.video.media.tracks.*
 import live.hms.video.utils.HMSCoroutineScope
 import live.hms.video.error.HMSException
 import com.facebook.react.modules.core.DeviceEventManagerModule
+import kotlinx.coroutines.launch
 import live.hms.video.sdk.HMSPreviewListener
 
 class HmsModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
@@ -181,9 +182,10 @@ class HmsModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaMod
   }
 
   @ReactMethod
-  fun switchCamera() {
-//    hmsSDK?.getLocalPeer()?.videoTrack?.switchCamera()
-    print("inside switchCamera")
+   fun switchCamera() {
+    HMSCoroutineScope.launch {
+      hmsSDK?.getLocalPeer()?.videoTrack?.switchCamera()
+    }
   }
 
   @ReactMethod
