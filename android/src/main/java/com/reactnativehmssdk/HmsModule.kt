ad2a34ab54419285e3f0cae0329a953e2169ec06
Samarth Jain <samarthjain@Samarth-JainI112s-MacBook-Pro.local>
2021-08-17 12:01:33 +0530

    dont switch camera is its off

--- android/src/main/java/com/reactnativehmssdk/HmsModule.kt
+++ android/src/main/java/com/reactnativehmssdk/HmsModule.kt
@@ -183,8 +183,12 @@ class HmsModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaMod
 
   @ReactMethod
    fun switchCamera() {
-    HMSCoroutineScope.launch {
-      hmsSDK?.getLocalPeer()?.videoTrack?.switchCamera()
+    if(hmsSDK?.getLocalPeer()?.videoTrack?.isMute?:true){
+      println("Camera off")
+    }else{
+      HMSCoroutineScope.launch {
+        hmsSDK?.getLocalPeer()?.videoTrack?.switchCamera()
+      }
     }
   }
 
