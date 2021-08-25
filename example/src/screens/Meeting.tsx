34ef1c0bd514318349a03b9d631a2701af9d0354
Samarth Jain <samarthjain@Samarth-JainI112s-MacBook-Pro.local>
2021-08-25 19:47:33 +0530

    fix: moved functions to utils

--- example/src/screens/Meeting.tsx
+++ example/src/screens/Meeting.tsx
@@ -23,6 +23,7 @@ import {useNavigation} from '@react-navigation/native';
 import dimension from '../utils/dimension';
 import type {StackNavigationProp} from '@react-navigation/stack';
 import type {AppStackParamList} from '../navigator';
+import {getRandomColor, getInitials} from '../utils/functions';
 
 type Peer = {
   trackId?: string;
@@ -52,32 +53,6 @@ type MeetingProps = {
 };
 
 type MeetingScreenProp = StackNavigationProp<AppStackParamList, 'Meeting'>;
-
-const getRandomColor = () => {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
-const getInitials = (name: String): String => {
-  let initials = '';
-  if (name.includes(' ')) {
-    const nameArray = name.split(' ');
-    if (nameArray[1].length > 0) {
-      initials = nameArray[0].substring(0, 1) + nameArray[1].substring(0, 1);
-    } else {
-      if (nameArray[0].length > 1) initials = nameArray[0].substring(0, 2);
-      else initials = nameArray[0].substring(0, 1);
-    }
-  } else {
-    if (name.length > 1) initials = name.substring(0, 2);
-    else initials = name.substring(0, 1);
-  }
-  return initials.toUpperCase();
-};
 
 const DisplayName = ({
   peerName,
