cfd84038f7b66a4f2f5f21196d4141a567a72e18
Lavi Moolchandani <lavim@geekyants.com>
2021-09-22 11:59:22 +0530

    feat: join with meeting link

--- example/src/services/index.ts
+++ example/src/services/index.ts
@@ -28,3 +28,30 @@ export const fetchToken = async ({
   const result = await response.json();
   return result;
 };
+
+export const fetchTokenFromLink = async ({
+  code,
+  subdomain,
+}: {
+  code: string;
+  subdomain: string;
+}) => {
+  const endPoint = 'https://prod-in.100ms.live/hmsapi/get-token';
+  const body = {
+    code,
+  };
+  const headers = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    subdomain,
+  };
+
+  const response = await fetch(endPoint, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers,
+  });
+
+  const result = await response.json();
+  return result;
+};
