3cf6e2e99e075184b023668f0663ee66d1da2744
Yogesh Singh <yogesh@100ms.live>
2021-10-01 20:04:14 +0530

    updated api-reference documentation

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,68 @@ import HMSConfig from './classes/HMSConfig';
 import HMSUpdateListenerActions from './classes/HMSUpdateListenerActions';
 import HMSMessage from './classes/HMSMessage';
 import HmsView from './classes/HmsView';
+import HMSVideoTrackSettings from './classes/HMSVideoTrackSettings';
+import HMSVideoTrack from './classes/HMSVideoTrack';
+import HMSVideoSettings from './classes/HMSVideoSettings';
+import HMSVideoCodec from './classes/HMSVideoCodec';
+import HMSTrackSettings from './classes/HMSTrackSettings';
+import HMSTrack from './classes/HMSTrack';
+import HMSSubscribeSettings from './classes/HMSSubscribeSettings';
+import HMSSimulcastSettings from './classes/HMSSimulcastSettings';
+import HMSSimulcastLayerSettings from './classes/HMSSimulcastLayerSettings';
+import HMSSDK from './classes/HMSSDK';
+import HMSRoom from './classes/HMSRoom';
+import HMSRoleChangeRequest from './classes/HMSRoleChangeRequest';
+import HMSRole from './classes/HMSRole';
+import HMSRemoteVideoTrack from './classes/HMSRemoteVideoTrack';
+import HMSRemotePeer from './classes/HMSRemotePeer';
+import HMSRemoteAudioTrack from './classes/HMSRemoteAudioTrack';
+import HMSPublishSettings from './classes/HMSPublishSettings';
+import HMSPermissions from './classes/HMSPermissions';
+import HMSPeer from './classes/HMSPeer';
+import HMSLocalVideoTrack from './classes/HMSLocalVideoTrack';
+import HMSLocalPeer from './classes/HMSLocalPeer';
+import HMSLocalAudioTrack from './classes/HMSLocalAudioTrack';
+import HMSHelper from './classes/HMSHelper';
+import HMSEncoder from './classes/HMSEncoder';
+import HMSAudioTrackSettings from './classes/HMSAudioTrackSettings';
+import HMSAudioTrack from './classes/HMSAudioTrack';
+import HMSAudioSettings from './classes/HMSAudioSettings';
+import HMSAudioCodec from './classes/HMSAudioCodec';
 
-export { HMSConfig, HmsView, HMSUpdateListenerActions, HMSMessage };
+export {
+  HMSConfig,
+  HmsView,
+  HMSUpdateListenerActions,
+  HMSMessage,
+  HMSVideoTrackSettings,
+  HMSVideoTrack,
+  HMSVideoSettings,
+  HMSVideoCodec,
+  HMSTrackSettings,
+  HMSTrack,
+  HMSSubscribeSettings,
+  HMSSimulcastSettings,
+  HMSSimulcastLayerSettings,
+  HMSSDK,
+  HMSRoom,
+  HMSRoleChangeRequest,
+  HMSRole,
+  HMSRemoteVideoTrack,
+  HMSRemotePeer,
+  HMSRemoteAudioTrack,
+  HMSPublishSettings,
+  HMSPermissions,
+  HMSPeer,
+  HMSLocalVideoTrack,
+  HMSLocalPeer,
+  HMSLocalAudioTrack,
+  HMSHelper,
+  HMSEncoder,
+  HMSAudioTrackSettings,
+  HMSAudioTrack,
+  HMSAudioSettings,
+  HMSAudioCodec,
+};
 
 export default HmsManager;
