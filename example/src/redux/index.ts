261bc91154d344d13de34fef0806209ede3f90ac
Samarth Jain <samarthjain@Samarth-JainI112s-MacBook-Pro.local>
2021-10-07 16:29:33 +0530

    fix: added types in reducers

--- example/src/redux/index.ts
+++ example/src/redux/index.ts
@@ -3,3 +3,6 @@ import {createStore} from 'redux';
 import rootReducer from './reducers/index';
 
 export const store = createStore(rootReducer);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
