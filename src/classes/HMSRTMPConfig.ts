ac4c7a1e8fb6bcc79ba0ac5c88b32a5ff254bb13
Lavi Moolchandani <lavim@geekyants.com>
2021-12-03 13:29:36 +0530

    added HMSRMTPConfig to ts classes in react-native package

---
+++ src/classes/HMSRTMPConfig.ts
@@ -0,0 +1,15 @@
+export class HMSConfig {
+  meetingURL?: string;
+  rtmpURLs?: [string];
+  record: boolean;
+
+  constructor(params: {
+    meetingURL?: string;
+    rtmpURLs?: [string];
+    record: boolean;
+  }) {
+    this.meetingURL = params.meetingURL;
+    this.rtmpURLs = params.rtmpURLs;
+    this.record = params.record;
+  }
+}
