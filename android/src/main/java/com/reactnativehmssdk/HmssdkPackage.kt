4fe09662a9bd1f2d276e9f64a2ee6aacf2da0fef
Samarth Jain <samarthjain@Samarth-JainI112s-MacBook-Pro.local>
2021-12-21 15:03:17 +0530

    feat: refactored HmssdkPackage

--- android/src/main/java/com/reactnativehmssdk/HmssdkPackage.kt
+++ android/src/main/java/com/reactnativehmssdk/HmssdkPackage.kt
@@ -4,7 +4,6 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import java.util.*
 
 class HmssdkPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
@@ -12,6 +11,6 @@ class HmssdkPackage : ReactPackage {
   }
 
   override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-    return Arrays.asList<ViewManager<*, *>>(HmssdkViewManager())
+    return listOf<ViewManager<*, *>>(HmssdkViewManager())
   }
 }
