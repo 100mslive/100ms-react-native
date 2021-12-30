9f1e2e2b740391c189b5e6f9f66bcb9b8b6de2d9
Samarth Jain <samarthjain@Samarth-JainI112s-MacBook-Pro.local>
2021-12-30 12:21:05 +0530

    fix custom role picker

--- example/src/screens/Meeting.tsx
+++ example/src/screens/Meeting.tsx
@@ -47,13 +47,7 @@ import Toast from 'react-native-simple-toast';
 import RNFetchBlob from 'rn-fetch-blob';
 import {Picker} from '@react-native-picker/picker';
 
-import {
-  ChatWindow,
-  AlertModal,
-  CustomModal,
-  CustomPicker,
-  RolePicker,
-} from '../components';
+import {ChatWindow, AlertModal, CustomModal, RolePicker} from '../components';
 import {
   addMessage,
   clearMessageData,
@@ -1336,7 +1330,7 @@ const Meeting = ({
         setModalVisible={setRoleModalVisible}
         title="Select action"
         buttons={getRoleRequestButtons()}>
-        <CustomPicker
+        <RolePicker
           data={instance?.knownRoles || []}
           selectedItem={newRole}
           onItemSelected={setNewRole}
