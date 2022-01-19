707528783f65eb65811b6d6d5af2a9a9aa340338
Yogesh Singh <yogesh@100ms.live>
2022-01-19 14:18:18 +0530

    setting tracks to nil on deinit

--- ios/HmsView.swift
+++ ios/HmsView.swift
@@ -139,4 +139,8 @@ class HmssdkDisplayView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    deinit {
+        videoView.setVideoTrack(nil)
+    }
 }
