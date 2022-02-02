43fdb81f9e4757efe25ec540fd2bed4f3985c5d9
Yogesh Singh <yogesh@100ms.live>
2022-02-02 16:56:40 +0530

    resolved swiftlint warnings

--- ios/HmsManager.swift
+++ ios/HmsManager.swift
@@ -1,11 +1,10 @@
-
 import HMSSDK
 
 @objc(HmsManager)
-class HmsManager: RCTEventEmitter{
-    
+class HmsManager: RCTEventEmitter {
+
     var hmsCollection: [String: HmsSDK] = [:]
-    
+
     let ON_PREVIEW = "ON_PREVIEW"
     let ON_JOIN = "ON_JOIN"
     let ON_ROOM_UPDATE = "ON_ROOM_UPDATE"
@@ -19,134 +18,133 @@ class HmsManager: RCTEventEmitter{
     let ON_SPEAKER = "ON_SPEAKER"
     let RECONNECTING = "RECONNECTING"
     let RECONNECTED = "RECONNECTED"
-    
+
     // MARK: - Setup
-    
+
     override init() {
         super.init()
     }
-    
+
 //    override class func requiresMainQueueSetup() -> Bool {
 //        true
 //    }
-    
+
     override func supportedEvents() -> [String]! {
         return [ON_JOIN, ON_PREVIEW, ON_ROOM_UPDATE, ON_PEER_UPDATE, ON_TRACK_UPDATE, ON_ERROR, ON_MESSAGE, ON_SPEAKER, RECONNECTING, RECONNECTED, ON_ROLE_CHANGE_REQUEST, ON_CHANGE_TRACK_STATE_REQUEST, ON_REMOVED_FROM_ROOM]
     }
-    
-    
+
     // MARK: - HMS SDK Actions
-    
+
     @objc
     func build(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         DispatchQueue.main.async { [weak self] in
             let hasItem = self?.hmsCollection.index(forKey: "12345")
-            
+
             if let _ = hasItem {
                 let id = UUID().uuidString
                 let hms = HmsSDK(data: data, delegate: self, uid: id)
                 self?.hmsCollection[id] = hms
-                
+
                 resolve?(id)
             } else {
                 let id = "12345"
                 let hms = HmsSDK(data: data, delegate: self, uid: id)
                 self?.hmsCollection[id] = hms
-                
+
                 resolve?(id)
             }
         }
     }
-    
+
     @objc
     func preview(_ credentials: NSDictionary) {
         let hms = HmsHelper.getHms(credentials, hmsCollection)
         hms?.preview(credentials)
     }
-    
+
     @objc
     func join(_ credentials: NSDictionary) {
         let hms = HmsHelper.getHms(credentials, hmsCollection)
         hms?.join(credentials)
     }
-    
+
     @objc
     func setLocalMute(_ data: NSDictionary) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.setLocalMute(data)
     }
-    
+
     @objc
     func setLocalVideoMute(_ data: NSDictionary) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.setLocalVideoMute(data)
     }
-    
+
     @objc
     func switchCamera(_ data: NSDictionary) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.switchCamera()
     }
-    
+
     @objc
     func leave(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.leave(resolve, reject)
     }
-    
+
     @objc
     func sendBroadcastMessage(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.sendBroadcastMessage(data, resolve, reject)
     }
-    
+
     @objc
     func sendGroupMessage(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.sendGroupMessage(data, resolve, reject)
     }
-    
+
     @objc
     func sendDirectMessage(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.sendDirectMessage(data, resolve, reject)
     }
-    
+
     @objc
     func acceptRoleChange(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.acceptRoleChange(resolve, reject)
     }
-    
+
     @objc
     func changeRole(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.changeRole(data, resolve, reject)
     }
-    
+
     @objc
     func changeTrackState(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.changeTrackState(data, resolve, reject)
     }
-    
+
     @objc
     func changeTrackStateForRoles(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.changeTrackStateForRoles(data, resolve, reject)
     }
-    
+
     @objc
     func isMute(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
@@ -155,102 +153,100 @@ class HmsManager: RCTEventEmitter{
         } else {
             reject?(nil, "NO_INSTANCE", nil)
             return
-        } 
+        }
     }
-    
-    
+
     @objc
     func removePeer(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.removePeer(data, resolve, reject)
     }
-    
-    
+
     @objc
     func endRoom(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.endRoom(data, resolve, reject)
     }
 
     @objc
     func setPlaybackAllowed(_ data: NSDictionary) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.setPlaybackAllowed(data)
     }
-    
+
     @objc
     func isPlaybackAllowed(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.isPlaybackAllowed(data, resolve, reject)
     }
-    
+
     @objc
     func getRoom(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.getRoom(data, resolve, reject)
     }
-    
+
     // MARK: - HMS SDK Delegate Callbacks
     func emitEvent(_ name: String, _ data: [String: Any]) {
         self.sendEvent(withName: name, body: data)
     }
-    
+
     // MARK: Helper Functions
-    
+
     @objc
     func muteAllPeersAudio(_ data: NSDictionary) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.muteAllPeersAudio(data)
     }
-    
+
     @objc
     func changeMetadata(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.changeMetadata(data, resolve, reject)
     }
 
     @objc
     func setVolume(_ data: NSDictionary) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.setVolume(data)
     }
-    
+
     @objc
     func startRTMPOrRecording(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.startRTMPOrRecording(data, resolve, reject)
     }
-    
+
     @objc
     func stopRtmpAndRecording(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.stopRtmpAndRecording(resolve, reject)
     }
-    
+
     @objc
     func startHLSStreaming(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.startHLSStreaming(data, resolve, reject)
     }
-    
+
     @objc
     func stopHLSStreaming(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
         let hms = HmsHelper.getHms(data, hmsCollection)
-        
+
         hms?.stopHLSStreaming(resolve, reject)
     }
-    
+
 //    @objc
 //    func setLocalVideoSettings(_ data: NSDictionary) {
 //        let hms = HmsHelper.getHms(data, hmsCollection)
--- ios/HmsView.swift
+++ ios/HmsView.swift
@@ -3,43 +3,43 @@ import AVKit
 
 @objc(HmsView)
 class HmsView: RCTViewManager {
-    
+
     override func view() -> (HmssdkDisplayView) {
         let view = HmssdkDisplayView()
         let hms = getHmsFromBridge()
-        
+
         view.setHms(hms)
-        
-        return view;
+
+        return view
     }
-    
+
     func getHmsFromBridge() -> [String: HmsSDK] {
         let collection: [String: HmsSDK] = (bridge.module(for: HmsManager.classForCoder()) as? HmsManager)?.hmsCollection ?? [:]
         return collection
     }
-    
+
     override class func requiresMainQueueSetup() -> Bool {
         true
     }
 }
 
 class HmssdkDisplayView: UIView {
-    
+
     lazy var videoView: HMSVideoView = {
         return HMSVideoView()
     }()
-    
+
     var hmsCollection: [String: HmsSDK] = [:]
     var sinked = false
     var sinkVideo = true
-    
+
     func setHms(_ hmsInstance: [String: HmsSDK]) {
         hmsCollection = hmsInstance
     }
-    
-    @objc var scaleType : String = "ASPECT_FILL" {
+
+    @objc var scaleType: String = "ASPECT_FILL" {
         didSet {
-            switch(scaleType) {
+            switch scaleType {
                 case "ASPECT_FIT":
                     videoView.videoContentMode = .scaleAspectFit
                     return
@@ -54,12 +54,12 @@ class HmssdkDisplayView: UIView {
             }
         }
     }
-    
+
     @objc var data: NSDictionary = [:] {
         didSet {
-            
+
             let sdkID = data.value(forKey: "id") as? String ?? "12345"
-            
+
             guard let hmsSDK = hmsCollection[sdkID]?.hms,
                   let trackID = data.value(forKey: "trackId") as? String,
                   let videoTrack = HMSUtilities.getVideoTrack(for: trackID, in: hmsSDK.room!),
@@ -68,14 +68,14 @@ class HmssdkDisplayView: UIView {
                 print(#function, "Required data to setup video view not found")
                 return
             }
-            
+
             sinkVideo = sink
-            
+
             let mirror = data.value(forKey: "mirror") as? Bool
-            if (mirror != nil) {
+            if mirror != nil {
                 videoView.mirror = mirror!
             }
-            
+
             if !sinked && sinkVideo {
                 videoView.setVideoTrack(videoTrack)
                 sinked = true
@@ -85,25 +85,25 @@ class HmssdkDisplayView: UIView {
             }
         }
     }
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(videoView)
         self.frame = frame
         self.backgroundColor = UIColor(displayP3Red: 0, green: 0, blue: 0, alpha: 1)
-        
+
         videoView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         videoView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         videoView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         videoView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         videoView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     deinit {
         videoView.setVideoTrack(nil)
     }
