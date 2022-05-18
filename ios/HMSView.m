93b4b266089bf9aef8c1377ccc1bf4574a940fae
Samarth Jain <samarthjain@192.168.1.42>
2022-05-18 13:17:19 +0530

    feat: added HMSView.m & HMSView.swift

---
+++ ios/HMSView.m
@@ -0,0 +1,9 @@
+#import "React/RCTViewManager.h"
+#import <Foundation/Foundation.h>
+
+@interface RCT_EXTERN_MODULE(HMSView, RCTViewManager)
+
+RCT_EXPORT_VIEW_PROPERTY(data, NSDictionary);
+RCT_EXPORT_VIEW_PROPERTY(scaleType, NSString)
+
+@end
