ba38d3e7d612cf0343b4aa07e79eae79a8eca2fc
Samarth Jain <samarthjain@192.168.1.2>
2022-10-17 13:40:08 +0530

    fix: display conetent edge to edge

--- example/android/app/src/main/java/live/hms/rn/MainActivity.java
+++ example/android/app/src/main/java/live/hms/rn/MainActivity.java
@@ -3,21 +3,39 @@ package live.hms.rn;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
     return "RNHMSExample";
   }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+    WindowInsetsControllerCompat windowInsetsController = ViewCompat
+        .getWindowInsetsController(getWindow().getDecorView());
+    if (windowInsetsController == null) {
+      return;
+    }
+    // Configure the behavior of the hidden system bars
+    windowInsetsController.setSystemBarsBehavior(
+        WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+    // Hide both the status bar and the navigation bar
+    windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
     getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
   }
 }
