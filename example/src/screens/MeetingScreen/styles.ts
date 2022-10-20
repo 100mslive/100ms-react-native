a517e011a54e49b2644f5e81cf206592358e2986
Samarth Jain <samarthjain@192.168.1.2>
2022-10-20 21:26:17 +0530

    fix: peer count overlapping iOS

--- example/src/screens/MeetingScreen/styles.ts
+++ example/src/screens/MeetingScreen/styles.ts
@@ -489,7 +489,7 @@ const styles = StyleSheet.create({
     borderColor: COLORS.BORDER.ACCENT,
     borderWidth: 2,
     borderRadius: 30,
-    marginLeft: 2,
+    marginLeft: 12,
     height: 30,
     paddingHorizontal: 6,
   },
