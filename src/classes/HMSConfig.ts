2b7004015b8a08c1156b898bbf1a86d0e9ba2c96
Jatin Nagar <jatin.nagar@100ms.live>
2023-04-04 16:56:33 +0530

    refactor(sdk): removed endpoint property from HMSConfig

--- src/classes/HMSConfig.ts
+++ src/classes/HMSConfig.ts
@@ -1,20 +1,17 @@
 export class HMSConfig {
   username: string;
   authToken: string;
-  endpoint?: string;
   metadata?: string;
   captureNetworkQualityInPreview?: boolean;
 
   constructor(params: {
     username: string;
     authToken: string;
-    endpoint?: string;
     metadata?: string;
     captureNetworkQualityInPreview?: boolean;
   }) {
     this.username = params.username;
     this.authToken = params.authToken;
-    this.endpoint = params.endpoint;
     this.metadata = params.metadata;
     this.captureNetworkQualityInPreview = params.captureNetworkQualityInPreview;
   }
