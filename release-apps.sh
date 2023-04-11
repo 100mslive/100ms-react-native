cf29c780aa734138ce5a4ec74bb95e12d7efd3b5
Yogesh Singh <yogesh@100ms.live>
2023-04-11 16:00:08 +0530

    updated Release Script

--- release-apps.sh
+++ release-apps.sh
@@ -4,76 +4,70 @@
 set -e
 # set -x
 
-perform_npm_actions()
-{
-    echo "testScript- 🌳🍀 git branch: $(git rev-parse --abbrev-ref HEAD)"
+perform_npm_actions() {
+  echo "testScript- 🌳🍀 git branch: $(git rev-parse --abbrev-ref HEAD)"
 
-    echo "testScript- perform_npm_actions $PWD"
+  echo "testScript- perform_npm_actions $PWD"
 
-    git pull --verbose
+  git pull --verbose
 
-    npm install
+  npm install
 
-    cd ./example
+  cd ./example
 
-    npm install
+  npm install
 
-    echo "testScript- perform_npm_actions $PWD"
+  echo "testScript- perform_npm_actions $PWD"
 }
 
-release_android()
-{
-    cd ./android
+release_android() {
+  cd ./android
 
-    # echo "release_android Android Android Android "
-    echo "testScript- release_android $PWD"
-    # echo "release_android Android Android Android "
+  # echo "release_android Android Android Android "
+  echo "testScript- release_android $PWD"
+  # echo "release_android Android Android Android "
 
-    bundle install --verbose
+  bundle install --verbose
 
-    bundle exec fastlane release_on_firebase
+  bundle exec fastlane release_on_firebase
 }
 
-release_iOS()
-{
-    cd ./ios
+release_iOS() {
+  cd ./ios
 
-    # echo "release_iOS iOS iOS iOS "
-    echo "testScript- release_iOS $PWD"
-    # echo "release_iOS iOS iOS iOS "
+  # echo "release_iOS iOS iOS iOS "
+  echo "testScript- release_iOS $PWD"
+  # echo "release_iOS iOS iOS iOS "
 
+  pod install --verbose
 
-    pod install --verbose
+  bundle install --verbose
 
-    bundle install --verbose
-
-    bundle exec fastlane distribute_app
+  bundle exec fastlane distribute_app
 }
 
-perform_git_actions()
-{
-    echo "testScript- perform_git_actions $PWD"
+perform_git_actions() {
+  echo "testScript- perform_git_actions $PWD"
 
-    cd ..
+  cd ..
 
-    while read line; do
-    if [[ $line =~ ^versionCode.[0-9]+$ ]]; then 
-        buildNumber=$(echo $line | grep -o -E '[0-9]+')
+  while read line; do
+    if [[ $line =~ ^versionCode.[0-9]+$ ]]; then
+      buildNumber=$(echo $line | grep -o -E '[0-9]+')
     elif [[ $line =~ ^versionName.*$ ]]; then
-        versionCode=$(echo $line | grep -o -E '[0-9].[0-9].[0-9]+')
+      versionCode=$(echo $line | grep -o -E '[0-9].[0-9].[0-9]+')
     fi
-    done <example/android/app/build.gradle
+  done <example/android/app/build.gradle
 
+  git add example/android/app/build.gradle
+  git add example/ios/Podfile.lock
+  git add example/ios/RNHMSExample/Info.plist
+  git add example/ios/RNHMSExample.xcodeproj/project.pbxproj
 
-    git add example/android/app/build.gradle
-    git add example/ios/Podfile.lock
-    git add example/ios/RNHMSExample/Info.plist
-    git add example/ios/RNHMSExample.xcodeproj/project.pbxproj
+  echo "testScript- perform_git_actions $PWD"
+  # git commit -m "released sample app version $versionCode ($buildNumber) ⚛️" --no-verify
 
-    echo "testScript- perform_git_actions $PWD"
-    # git commit -m "released sample app version $versionCode ($buildNumber) ⚛️" --no-verify
-
-    # git push --verbose
+  # git push --verbose
 }
 
 perform_npm_actions
