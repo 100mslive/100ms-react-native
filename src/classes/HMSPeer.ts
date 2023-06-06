8b028bc8c171d884fd3542daf434740c094b41b8
Jatin Nagar <jatin.nagar@100ms.live>
2023-06-06 16:59:52 +0530

    refactor(sdk): caching more properties at HMSPeer object level

--- src/classes/HMSPeer.ts
+++ src/classes/HMSPeer.ts
@@ -9,21 +9,43 @@ import { HMSConstants } from './HMSConstants';
 export class HMSPeer {
   peerID: string;
   private _name: string = '';
-  private _isLocal: boolean | undefined = undefined;
+  private _isLocal: boolean | undefined;
+  private _customerUserID: string | undefined;
+  private _metadata: string | undefined;
+  private _role: HMSRole | undefined;
 
   private _updateName(value: string) {
-    // If given value is something valid, and `_name` is outdated
-    // Update `_name`
-    if (value && this._name !== value) {
+    // If `_name` is outdated, update it
+    if (this._name !== value) {
       this._name = value;
     }
   }
 
   private _updateIsLocal(value: boolean) {
-    // If given value is valid, and `_isLocal` is outdated
-    // Update `_isLocal`
+    // If `_isLocal` is outdated, update it
     if (this._isLocal !== value) {
       this._isLocal = value;
+    }
+  }
+
+  private _updateCustomerUserID(value: string) {
+    // If `_customerUserID` is outdated, update it
+    if (this._customerUserID !== value) {
+      this._customerUserID = value;
+    }
+  }
+
+  private _updateMetadata(value: string) {
+    // If `_metadata` is outdated, update it
+    if (this._metadata !== value) {
+      this._metadata = value;
+    }
+  }
+
+  private _updateRole(value: HMSRole) {
+    // If `_role` is outdated, update it
+    if (this._role !== value) {
+      this._role = value;
     }
   }
 
@@ -34,48 +56,35 @@ export class HMSPeer {
   get name(): string {
     const hmsPeersCache = getHmsPeersCache();
 
-    if (hmsPeersCache) {
-      const nameValue = hmsPeersCache.getProperty(this.peerID, 'name') || '';
+    const nameValue = hmsPeersCache
+      ? hmsPeersCache.getProperty(this.peerID, 'name')
+      : getPeerPropertyFromNative(
+          HMSConstants.DEFAULT_SDK_ID,
+          this.peerID,
+          'name'
+        );
 
+    if (nameValue) {
       this._updateName(nameValue);
-
-      return nameValue ?? this._name;
     }
-
-    const value =
-      getPeerPropertyFromNative(
-        HMSConstants.DEFAULT_SDK_ID,
-        this.peerID,
-        'name'
-      ) || '';
-
-    this._updateName(value);
-
-    return value ?? this._name;
+    return nameValue ?? this._name;
   }
 
   get isLocal(): boolean | undefined {
     const hmsPeersCache = getHmsPeersCache();
 
-    if (hmsPeersCache) {
-      const value = hmsPeersCache.getProperty(this.peerID, 'isLocal');
-
-      if (typeof value === 'boolean') {
-        this._updateIsLocal(value);
-      }
-      return value ?? this._isLocal;
-    }
-
-    const value = getPeerPropertyFromNative(
-      HMSConstants.DEFAULT_SDK_ID,
-      this.peerID,
-      'isLocal'
-    );
+    const value = hmsPeersCache
+      ? hmsPeersCache.getProperty(this.peerID, 'isLocal')
+      : getPeerPropertyFromNative(
+          HMSConstants.DEFAULT_SDK_ID,
+          this.peerID,
+          'isLocal'
+        );
 
     if (typeof value === 'boolean') {
       this._updateIsLocal(value);
     }
-    return value || this._isLocal;
+    return value ?? this._isLocal;
   }
 
   get networkQuality(): HMSNetworkQuality | undefined {
@@ -95,43 +104,52 @@ export class HMSPeer {
   get customerUserID(): string | undefined {
     const hmsPeersCache = getHmsPeersCache();
 
-    if (hmsPeersCache) {
-      return hmsPeersCache.getProperty(this.peerID, 'customerUserID');
-    }
+    const value = hmsPeersCache
+      ? hmsPeersCache.getProperty(this.peerID, 'customerUserID')
+      : getPeerPropertyFromNative(
+          HMSConstants.DEFAULT_SDK_ID,
+          this.peerID,
+          'customerUserID'
+        );
 
-    return getPeerPropertyFromNative(
-      HMSConstants.DEFAULT_SDK_ID,
-      this.peerID,
-      'customerUserID'
-    );
+    if (value) {
+      this._updateCustomerUserID(value);
+    }
+    return value ?? this._customerUserID;
   }
 
   get metadata(): string | undefined {
     const hmsPeersCache = getHmsPeersCache();
 
-    if (hmsPeersCache) {
-      return hmsPeersCache.getProperty(this.peerID, 'metadata');
-    }
+    const value = hmsPeersCache
+      ? hmsPeersCache.getProperty(this.peerID, 'metadata')
+      : getPeerPropertyFromNative(
+          HMSConstants.DEFAULT_SDK_ID,
+          this.peerID,
+          'metadata'
+        );
 
-    return getPeerPropertyFromNative(
-      HMSConstants.DEFAULT_SDK_ID,
-      this.peerID,
-      'metadata'
-    );
+    if (value) {
+      this._updateMetadata(value);
+    }
+    return value ?? this._metadata;
   }
 
   get role(): HMSRole | undefined {
     const hmsPeersCache = getHmsPeersCache();
 
-    if (hmsPeersCache) {
-      return hmsPeersCache.getProperty(this.peerID, 'role');
-    }
+    const value = hmsPeersCache
+      ? hmsPeersCache.getProperty(this.peerID, 'role')
+      : getPeerPropertyFromNative(
+          HMSConstants.DEFAULT_SDK_ID,
+          this.peerID,
+          'role'
+        );
 
-    return getPeerPropertyFromNative(
-      HMSConstants.DEFAULT_SDK_ID,
-      this.peerID,
-      'role'
-    );
+    if (value) {
+      this._updateRole(value);
+    }
+    return value ?? this._role;
   }
 
   get audioTrack(): HMSAudioTrack | undefined {
