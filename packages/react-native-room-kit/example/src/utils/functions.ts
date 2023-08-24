886b6404ac1f0d6e27540c736befb42baaa2174b
Yogesh Singh <yogesh@100ms.live>
2023-08-24 17:28:28 +0530

    updated default meeting link

--- packages/react-native-room-kit/example/src/utils/functions.ts
+++ packages/react-native-room-kit/example/src/utils/functions.ts
@@ -9,7 +9,7 @@ import {
 import { getRoomLinkDetails } from './getRoomLinkDetails';
 
 export const getMeetingUrl = () =>
-  'https://saikat.qa-app.100ms.live/meeting/jnf-wqy-par';
+  'https://public.app.100ms.live/meeting/xvm-wxwo-gbl';
 
 export const callService = async (
   roomID: string,
