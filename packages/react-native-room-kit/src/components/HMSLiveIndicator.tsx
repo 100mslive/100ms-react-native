91f127e50fe8ace8327b0673e71123f554f0cc33
ygit <ygit@users.noreply.github.com>
2023-09-06 18:39:45 +0530

    Update packages/react-native-room-kit/src/components/HMSLiveIndicator.tsx
    Co-authored-by: Jatin Nagar <jatin.nagar@100ms.live>

--- packages/react-native-room-kit/src/components/HMSLiveIndicator.tsx
+++ packages/react-native-room-kit/src/components/HMSLiveIndicator.tsx
@@ -51,7 +51,14 @@ const _HMSLiveIndicator = () => {
   }));
 
   const showParticipantList = () => {
-    setModalVisible(ModalTypes.PARTICIPANTS);
+      if (isHLSViewer) {
+        setModalVisible(ModalTypes.PARTICIPANTS);
+      } else {
+        batch(() => {
+          dispatch(setActiveChatBottomSheetTab('Participants'));
+          dispatch({ type: 'SET_SHOW_CHAT_VIEW', showChatView: true });
+        });
+      }
   };
 
   if (!live) {
