e5072aec51f3c6fd8552504e132410aa46ee5acc
Yogesh Singh <yogesh@100ms.live>
2023-09-07 16:13:40 +0530

    added defaults values for hls streaming state

--- packages/react-native-room-kit/src/hooks-util-selectors.tsx
+++ packages/react-native-room-kit/src/hooks-util-selectors.tsx
@@ -41,10 +41,10 @@ export const selectShouldGoLive = (state: RootState) => {
     currentRole
   );
 
-  const isHLSStreaming = state.hmsStates.room?.hlsStreamingState.running;
+  const isHLSStreaming = state.hmsStates.room?.hlsStreamingState?.running ?? false;
 
   const joinButtonType =
-    layoutConfig?.screens?.preview?.default?.elements?.join_form?.join_btn_type;
+    layoutConfig?.screens?.preview?.default?.elements?.join_form?.join_btn_type ?? JoinForm_JoinBtnType.JOIN_BTN_TYPE_JOIN_ONLY;
 
   const canStartHLSStreaming =
     joinButtonType === JoinForm_JoinBtnType.JOIN_BTN_TYPE_JOIN_AND_GO_LIVE ||
