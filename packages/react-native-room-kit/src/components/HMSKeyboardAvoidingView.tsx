f9fe91b104a52526a075a5947adc1448576c24aa
Jatin Nagar <jatin.nagar@100ms.live>
2023-10-17 16:07:57 +0530

    fix(prebuilt): :bug: fixed bottom offset of Keyboard avoiding Inset ChatView

--- packages/react-native-room-kit/src/components/HMSKeyboardAvoidingView.tsx
+++ packages/react-native-room-kit/src/components/HMSKeyboardAvoidingView.tsx
@@ -33,9 +33,7 @@ export const HMSKeyboardAvoidingView: React.FC<
   });
 
   return (
-    <Animated.View
-      style={[style, keyboardAvoidStyle]}
-    >
+    <Animated.View style={[style, keyboardAvoidStyle]}>
       {children}
     </Animated.View>
   );
