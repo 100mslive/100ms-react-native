249909f6933aabc4ca2a4c367dc8b2bfb91d70b9
Yogesh Singh <yogesh@100ms.live>
2023-11-21 13:34:57 +0530

    removed stopped_at property from RTMP streaming state

--- packages/react-native-hms/src/classes/HMSRtmpStreamingState.ts
+++ packages/react-native-hms/src/classes/HMSRtmpStreamingState.ts
@@ -5,20 +5,17 @@ export class HMSRtmpStreamingState {
   running: boolean;
   error?: HMSException;
   startedAt?: Date;
-  stoppedAt?: Date;
   state: HMSStreamingState;
 
   constructor(params: {
     running: boolean;
     error?: HMSException;
     startedAt?: Date;
-    stoppedAt?: Date;
     state: HMSStreamingState;
   }) {
     this.running = params.running;
     this.error = params.error;
     this.startedAt = params.startedAt;
-    this.stoppedAt = params.stoppedAt;
     this.state = params.state;
   }
 }
