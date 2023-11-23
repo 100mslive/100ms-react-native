c958523a942f2bb7559b410024c3f2d260b48dbb
Jatin Nagar <jatin.nagar@100ms.live>
2023-11-23 17:01:56 +0530

    fix(prebuilt): :bug: text input stuck on keyboard opened position
    this happens when modal is opened while keyboard is already open

--- packages/react-native-room-kit/src/components/HMSKeyboardAvoidingView.tsx
+++ packages/react-native-room-kit/src/components/HMSKeyboardAvoidingView.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import { Keyboard, Platform } from 'react-native';
 import type { StyleProp, ViewStyle } from 'react-native';
 import Animated, {
+  KeyboardState,
   useAnimatedKeyboard,
   useAnimatedStyle,
   useDerivedValue,
+  useSharedValue,
 } from 'react-native-reanimated';
 
 export interface HMSKeyboardAvoidingViewProps {
@@ -15,6 +18,61 @@ export const HMSKeyboardAvoidingView: React.FC<
   HMSKeyboardAvoidingViewProps
 > = ({ children, style, bottomOffset=0 }) => {
   const animatedKeyboard = useAnimatedKeyboard();
+  const keyboardState = useSharedValue(KeyboardState.UNKNOWN);
+
+  React.useEffect(() => {
+    if (Platform.OS === 'android') {
+      let didShowTimeoutId: null | NodeJS.Timeout = null;
+      let didHideTimeoutId: null | NodeJS.Timeout = null;
+
+      const didShowSubscription = Keyboard.addListener(
+        'keyboardDidShow',
+        () => {
+          keyboardState.value = KeyboardState.OPENING;
+          if (didShowTimeoutId !== null) {
+            clearTimeout(didShowTimeoutId);
+          }
+          didShowTimeoutId = setTimeout(() => {
+            keyboardState.value = KeyboardState.OPEN;
+            didShowTimeoutId = null;
+          }, 400);
+        }
+      );
+
+      const didHideSubscription = Keyboard.addListener(
+        'keyboardDidHide',
+        () => {
+          keyboardState.value = KeyboardState.CLOSING;
+          if (didHideTimeoutId !== null) {
+            clearTimeout(didHideTimeoutId);
+          }
+          didHideTimeoutId = setTimeout(() => {
+            keyboardState.value = KeyboardState.CLOSED;
+            didHideTimeoutId = null;
+          }, 400);
+        }
+      );
+
+      return () => {
+        if (didShowTimeoutId !== null) {
+          clearTimeout(didShowTimeoutId);
+        }
+        if (didHideTimeoutId !== null) {
+          clearTimeout(didHideTimeoutId);
+        }
+        if ('remove' in didShowSubscription) {
+          didShowSubscription.remove();
+        } else {
+          Keyboard.removeSubscription(didShowSubscription);
+        }
+        if ('remove' in didHideSubscription) {
+          didHideSubscription.remove();
+        } else {
+          Keyboard.removeSubscription(didHideSubscription);
+        }
+      };
+    }
+  }, [animatedKeyboard, keyboardState]);
 
   const initialPageY = useDerivedValue(() => bottomOffset, [bottomOffset]);
 
@@ -24,7 +82,8 @@ export const HMSKeyboardAvoidingView: React.FC<
       transform: [
         {
           translateY:
-            keyboardHeight <= initialPageY.value
+            keyboardHeight <= initialPageY.value ||
+            keyboardState.value === KeyboardState.CLOSED
               ? 0 // Keep element at original `pageY` till and when keyboard height is less than `pageY`
               : -(keyboardHeight - initialPageY.value),
         },
