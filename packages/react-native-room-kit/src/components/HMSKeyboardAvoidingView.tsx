7303e9d7530d5abc58f1d305d5bd159eadc52422
Jatin Nagar <jatin.nagar@100ms.live>
2023-12-19 22:52:03 +0530

    using reusable `useKeyboardState` hook

--- packages/react-native-room-kit/src/components/HMSKeyboardAvoidingView.tsx
+++ packages/react-native-room-kit/src/components/HMSKeyboardAvoidingView.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import { Keyboard, Platform } from 'react-native';
 import type { StyleProp, ViewStyle } from 'react-native';
 import Animated, {
   KeyboardState,
   useAnimatedKeyboard,
   useAnimatedStyle,
   useDerivedValue,
-  useSharedValue,
 } from 'react-native-reanimated';
 import type { SharedValue } from 'react-native-reanimated';
+
+import { useKeyboardState } from '../hooks-util';
 
 export interface HMSKeyboardAvoidingViewProps {
   style?: StyleProp<Animated.AnimateStyle<StyleProp<ViewStyle>>>;
@@ -19,61 +19,7 @@ export const HMSKeyboardAvoidingView: React.FC<
   HMSKeyboardAvoidingViewProps
 > = ({ children, style, bottomOffset = 0 }) => {
   const animatedKeyboard = useAnimatedKeyboard();
-  const keyboardState = useSharedValue(KeyboardState.UNKNOWN);
-
-  React.useEffect(() => {
-    if (Platform.OS === 'android') {
-      let didShowTimeoutId: null | NodeJS.Timeout = null;
-      let didHideTimeoutId: null | NodeJS.Timeout = null;
-
-      const didShowSubscription = Keyboard.addListener(
-        'keyboardDidShow',
-        () => {
-          keyboardState.value = KeyboardState.OPENING;
-          if (didShowTimeoutId !== null) {
-            clearTimeout(didShowTimeoutId);
-          }
-          didShowTimeoutId = setTimeout(() => {
-            keyboardState.value = KeyboardState.OPEN;
-            didShowTimeoutId = null;
-          }, 400);
-        }
-      );
-
-      const didHideSubscription = Keyboard.addListener(
-        'keyboardDidHide',
-        () => {
-          keyboardState.value = KeyboardState.CLOSING;
-          if (didHideTimeoutId !== null) {
-            clearTimeout(didHideTimeoutId);
-          }
-          didHideTimeoutId = setTimeout(() => {
-            keyboardState.value = KeyboardState.CLOSED;
-            didHideTimeoutId = null;
-          }, 400);
-        }
-      );
-
-      return () => {
-        if (didShowTimeoutId !== null) {
-          clearTimeout(didShowTimeoutId);
-        }
-        if (didHideTimeoutId !== null) {
-          clearTimeout(didHideTimeoutId);
-        }
-        if ('remove' in didShowSubscription) {
-          didShowSubscription.remove();
-        } else {
-          Keyboard.removeSubscription(didShowSubscription);
-        }
-        if ('remove' in didHideSubscription) {
-          didHideSubscription.remove();
-        } else {
-          Keyboard.removeSubscription(didHideSubscription);
-        }
-      };
-    }
-  }, [animatedKeyboard, keyboardState]);
+  const { keyboardState } = useKeyboardState();
 
   const initialPageY = useDerivedValue(() => {
     return typeof bottomOffset === 'number' ? bottomOffset : bottomOffset.value;
