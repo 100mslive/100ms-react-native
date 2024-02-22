9da27fc6b595843941e8326f3174edb3166c3d29
Jatin Nagar <jatin.nagar@100ms.live>
2024-02-22 16:38:10 +0530

    feat(sdk): :sparkles: added `fetchLeaderboard` api in js and android sdk

--- packages/react-native-hms/android/src/main/java/com/reactnativehmssdk/Interactivity/HMSRNInteractivityCenter.kt
+++ packages/react-native-hms/android/src/main/java/com/reactnativehmssdk/Interactivity/HMSRNInteractivityCenter.kt
@@ -1,5 +1,4 @@
 package com.reactnativehmssdk
-import android.util.Log
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReadableMap
@@ -10,6 +9,7 @@ import live.hms.video.interactivity.HmsPollUpdateListener
 import live.hms.video.polls.models.HMSPollUpdateType
 import live.hms.video.polls.models.HmsPoll
 import live.hms.video.polls.models.answer.PollAnswerResponse
+import live.hms.video.polls.network.PollLeaderboardResponse
 import live.hms.video.sdk.HMSActionResultListener
 import live.hms.video.sdk.HMSSDK
 import live.hms.video.sdk.HmsTypedActionResultListener
@@ -37,8 +37,6 @@ class HMSRNInteractivityCenter(private val sdk: HMSSDK, private val rnSDK: HMSRN
       }
   }
 
-  // region Create Polls
-
   fun quickStartPoll(
     data: ReadableMap,
     promise: Promise?,
@@ -58,41 +56,6 @@ class HMSRNInteractivityCenter(private val sdk: HMSSDK, private val rnSDK: HMSRN
       },
     )
   }
-
-  /*
-
-  func add(_ data: NSDictionary, _ resolve: RCTPromiseResolveBlock?, _ reject: RCTPromiseRejectBlock?) {
-        guard let pollId = data["pollId"] as? String,
-              let poll = self.hmssdk?.interactivityCenter.polls.first(where: {poll in poll.pollID == pollId}) else {
-            reject?("6004", "Unable to find HMSPoll with given pollId", nil)
-            return
-        }
-        guard let pollQuestionIndex = data["pollQuestionIndex"] as? Int,
-              let pollQuestion = poll.questions?.first(where: {question in question.index == pollQuestionIndex}) else {
-            reject?("6004", "Unable to find HMSPollQuestion in poll with given question index", nil)
-            return
-        }
-        guard let responses = data["responses"] as? NSDictionary else {
-            reject?("6004", "responses field is required", nil)
-            return
-        }
-
-        let pollResponseBuilder = HMSInteractivityHelper.getPollResponseBuilder(responses, poll: poll, pollQuestion: pollQuestion)
-
-        self.hmssdk?.interactivityCenter.add(response: pollResponseBuilder) { pollQuestionResponseResult, error in
-            if let nonnilError = error {
-                reject?("6004", nonnilError.localizedDescription, nil)
-                return
-            }
-            if let pollQuestionResponseResult = pollQuestionResponseResult {
-                resolve?(HMSInteractivityDecoder.getHMSPollQuestionResponseResults(pollQuestionResponseResult))
-            } else {
-                resolve?(nil)
-            }
-        }
-    }
-
-   */
 
   fun addResponseOnPollQuestion(
     data: ReadableMap,
@@ -162,23 +125,38 @@ class HMSRNInteractivityCenter(private val sdk: HMSSDK, private val rnSDK: HMSRN
     )
   }
 
-  // endregion
+  fun fetchLeaderboard(
+    data: ReadableMap,
+    promise: Promise?,
+  ) {
+    val pollId = data.getString("pollId")
+    if (pollId == null) {
+      promise?.reject("6002", "pollId is required")
+      return
+    }
+    val poll = this.sdk.getHmsInteractivityCenter().polls.find { it.pollId == pollId }
+    if (poll == null) {
+      promise?.reject("6002", "No HMSPoll with pollId `$pollId`")
+      return
+    }
+    val count = data.getInt("count")
+    val startIndex = data.getInt("startIndex")
+    val includeCurrentPeer = data.getBoolean("includeCurrentPeer")
 
-  // region Poll Update Listener
+    this.sdk.getHmsInteractivityCenter().fetchLeaderboard(
+      pollId = poll.pollId,
+      count = count.toLong(),
+      startIndex = startIndex.toLong(),
+      includeCurrentPeer = includeCurrentPeer,
+      object : HmsTypedActionResultListener<PollLeaderboardResponse> {
+        override fun onSuccess(result: PollLeaderboardResponse) {
+          promise?.resolve(HMSInteractivityDecoder.getPollLeaderboardResponse(result))
+        }
 
-  fun showPollStartedToast() {
-    // Show toast
-    Log.e("Interactivity", "showPollStartedToast")
+        override fun onError(error: HMSException) {
+          promise?.reject(error.code.toString(), error.description)
+        }
+      },
+    )
   }
-
-  fun loadResultsSummaryIfNeeded() {
-    // Load results summary
-    Log.e("Interactivity", "loadResultsSummaryIfNeeded")
-  }
-
-  fun updateResultsScreen() {
-    // Update results screen
-    Log.e("Interactivity", "updateResultsScreen")
-  }
-  // endregion
 }
