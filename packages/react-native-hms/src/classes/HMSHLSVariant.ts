cc8931ae785af1845c95c79364ce165de4bd9f2a
ygit <ygit@users.noreply.github.com>
2024-04-18 20:32:55 +0530

    RN-274: added DVR / noDVR playlist type to HLS Variant (#1384)
    # Description
    
    - added DVR / noDVR playlist type to HLS Variant
    
    ### Pre-launch Checklist
    
    - [x] The [Documentation] is updated accordingly, or this PR doesn't
    require it.
    - [x] I have updated the `ExampleAppChangelog.txt` file with relevant
    changes.
    - [x] I updated/added relevant documentation (doc comments with `///`).
    - [x] I listed at least one issue that this PR fixes in the description
    above.
    - [x] I added new tests to check the change I am making, or this PR is
    test-exempt.
    - [x] All existing and new tests are passing.
    
    <!-- Links -->
    
    [Documentation]: https://www.100ms.live/docs

--- packages/react-native-hms/src/classes/HMSHLSVariant.ts
+++ packages/react-native-hms/src/classes/HMSHLSVariant.ts
@@ -1,18 +1,23 @@
+import type { HMSHLSPlaylistType } from './HMSHLSPlaylistType';
+
 export class HMSHLSVariant {
   hlsStreamUrl?: string;
   meetingUrl?: string;
   metadata?: string;
   startedAt?: Date;
+  playlistType?: HMSHLSPlaylistType;
 
   constructor(params: {
     hlsStreamUrl?: string;
     meetingUrl?: string;
     metadata?: string;
     startedAt?: Date;
+    playlistType?: HMSHLSPlaylistType;
   }) {
     this.hlsStreamUrl = params.hlsStreamUrl;
     this.meetingUrl = params.meetingUrl;
     this.metadata = params.metadata;
     this.startedAt = params.startedAt;
+    this.playlistType = params.playlistType;
   }
 }
