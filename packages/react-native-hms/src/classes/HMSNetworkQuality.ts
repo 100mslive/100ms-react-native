028c1445e37a5bd3572b23dbb07d7a3a59a2e85e
Yogesh Singh <yogesh@100ms.live>
2024-07-17 15:46:07 +0530

    updated inline code comments

--- packages/react-native-hms/src/classes/HMSNetworkQuality.ts
+++ packages/react-native-hms/src/classes/HMSNetworkQuality.ts
@@ -1,3 +1,14 @@
+/**
+ * Represents the network quality of a connection in the HMS (100ms) system.
+ *
+ * This class encapsulates the network quality information, specifically focusing on the downlink quality of the connection.
+ * The downlink quality is represented as a numerical value, providing a quantifiable measure of the receiving end's network performance.
+ *
+ * @param {Object} params - The constructor parameters.
+ * @param {number} params.downlinkQuality - The downlink quality of the network connection.
+ *
+ * @see https://www.100ms.live/docs/react-native/v2/how-to-guides/measure-network-quality-and-performance/network-quality
+ */
 export class HMSNetworkQuality {
   downlinkQuality: number;
 
--- packages/react-native-hms/src/classes/HMSVideoTrack.ts
+++ packages/react-native-hms/src/classes/HMSVideoTrack.ts
@@ -2,7 +2,24 @@ import { HMSTrack } from './HMSTrack';
 import type { HMSTrackSource } from './HMSTrackSource';
 import type { HMSTrackType } from './HMSTrackType';
 
+/**
+ * Represents a video track within the HMS (100ms) system.
+ *
+ * This class extends `HMSTrack` to provide functionalities specific to video tracks. It includes additional
+ * property `isDegraded` to indicate whether the video quality has been intentionally reduced, typically for
+ * performance reasons. The constructor allows for the initialization of the video track with various parameters
+ * such as track ID, source, description, mute state, degradation state, ID, and type. These parameters provide
+ * detailed information about the video track and its characteristics.
+ *
+ * @extends HMSTrack
+ *
+ * @see https://www.100ms.live/docs/react-native/v2/how-to-guides/set-up-video-conferencing/render-video/overview
+ */
 export class HMSVideoTrack extends HMSTrack {
+  /**
+   * Indicates whether the video quality of the track is degraded.
+   * @type {boolean | undefined}
+   */
   isDegraded?: boolean;
 
   constructor(params: {
