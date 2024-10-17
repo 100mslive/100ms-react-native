c2407d82938fdf8d7337fb3d986b4ff1cfe7672b
Yogesh Singh <yogesh@100ms.live>
2024-10-17 14:25:43 +0530

    removed foreground notification service from android

--- packages/react-native-room-kit/example/src/screens/HMSPrebuiltScreen/index.tsx
+++ packages/react-native-room-kit/example/src/screens/HMSPrebuiltScreen/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useMemo } from 'react';
-import { Platform, Text } from 'react-native';
+import { Platform, Text, View } from 'react-native';
 import {
   NavigationProp,
   RouteProp,
@@ -14,8 +14,6 @@ import type {
 } from '@100mslive/react-native-room-kit';
 
 import { AppStackParamList } from '../../navigator';
-
-import VIForegroundService from '@voximplant/react-native-foreground-service';
 
 export const HMSPrebuiltScreen = () => {
   const navigation = useNavigation<NavigationProp<AppStackParamList>>();
@@ -36,9 +34,6 @@ export const HMSPrebuiltScreen = () => {
    */
   const handleMeetingLeave: OnLeaveHandler = useCallback(async (reason) => {
     console.log(':: reason > ', reason); // Logs the reason for leaving the meeting.
-    if (Platform.OS === 'android') {
-      await VIForegroundService.getInstance().stopService(); // Stops the foreground service on Android.
-    }
     navigation.navigate('QRCodeScreen'); // Navigates to the QRCodeScreen.
   }, []);
 
@@ -74,48 +69,6 @@ export const HMSPrebuiltScreen = () => {
     }),
     [screenParams] // Recomputes the memoized value when screenParams changes.
   );
-
-  useEffect(() => {
-    // Checks if the platform is Android before attempting to start the foreground service.
-    // This is necessary because the foreground service functionality is specific to Android.
-    if (Platform.OS === 'android') {
-      // Defines an asynchronous function to start the Android foreground service.
-      const startAndroidForegroundService = async () => {
-        // Configuration for the Android notification channel.
-        // This is required for Android O and above to show notifications.
-        const androidForegroundServiceChannelConfig = {
-          id: 'MyAppChannelID', // Unique ID for the notification channel.
-          name: 'My App Channel name', // Name for the notification channel.
-          enableVibration: true, // Enables vibration for notifications from this channel.
-          importance: 5, // Sets the importance level for notifications from this channel.
-        };
-        // Creates the notification channel with the specified configuration.
-        await VIForegroundService.getInstance().createNotificationChannel(
-          androidForegroundServiceChannelConfig
-        );
-
-        // Configuration for the notification that will be shown while the service is running.
-        const notificationConfig = {
-          channelId: 'MyAppChannelID', // Associates the notification with the created channel ID.
-          id: 1000, // Unique ID for the notification.
-          title: 'Foreground Service', // Title for the notification.
-          text: 'Starting Android Foreground Service now', // Text content for the notification.
-          icon: '../assets/100ms-logo.png', // Icon for the notification.
-          priority: 2, // Priority for the notification.
-        };
-        // Attempts to start the foreground service with the specified notification configuration.
-        try {
-          VIForegroundService.getInstance().startService(notificationConfig);
-        } catch (e) {
-          // Logs an error if starting the foreground service fails.
-          console.error(e);
-        }
-      };
-
-      // Calls the function to start the Android foreground service.
-      startAndroidForegroundService();
-    }
-  }, []); // The empty dependency array ensures this effect runs only once after the initial render.
 
   // Room Code is required to join the room
   if (!roomCode) {
